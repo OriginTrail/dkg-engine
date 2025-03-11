6009eb22975259338294351351fd1c4b0f622133
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-03-11 10:16:36 +0100

    Remove UPDATE emit change events

--- src/commands/protocols/update/sender/v1.0.0/v1-0-0-update-request-command.js
+++ src/commands/protocols/update/sender/v1.0.0/v1-0-0-update-request-command.js
@@ -2,7 +2,6 @@ import ProtocolRequestCommand from '../../../common/protocol-request-command.js'
 import {
     NETWORK_MESSAGE_TIMEOUT_MILLS,
     ERROR_TYPE,
-    OPERATION_ID_STATUS,
     NETWORK_SIGNATURES_FOLDER,
 } from '../../../../../constants/constants.js';
 
@@ -13,14 +12,6 @@ class PublishRequestCommand extends ProtocolRequestCommand {
         this.signatureService = ctx.signatureService;
         this.operationIdService = ctx.operationIdService;
         this.errorType = ERROR_TYPE.UPDATE.UPDATE_STORE_REQUEST_ERROR;
-
-        this.operationStartEvent = OPERATION_ID_STATUS.UPDATE.UPDATE_REQUEST_START;
-        this.operationEndEvent = OPERATION_ID_STATUS.UPDATE.UPDATE_REQUEST_END;
-        this.prepareMessageStartEvent =
-            OPERATION_ID_STATUS.UPDATE.UPDATE_REQUEST_PREPARE_MESSAGE_START;
-        this.prepareMessageEndEvent = OPERATION_ID_STATUS.UPDATE.UPDATE_REQUEST_PREPARE_MESSAGE_END;
-        this.sendMessageStartEvent = OPERATION_ID_STATUS.UPDATE.UPDATE_SEND_MESSAGE_START;
-        this.sendMessageEndEvent = OPERATION_ID_STATUS.UPDATE.UPDATE_SEND_MESSAGE_END;
     }
 
     async prepareMessage(command) {
@@ -29,17 +20,7 @@ class PublishRequestCommand extends ProtocolRequestCommand {
         // TODO: Backwards compatibility, send blockchain without chainId
         const { blockchain } = command.data;
 
-        await this.operationIdService.emitChangeEvent(
-            OPERATION_ID_STATUS.UPDATE.UPDATE_GET_CACHED_OPERATION_ID_DATA_START,
-            operationId,
-            blockchain,
-        );
         const { dataset } = await this.operationIdService.getCachedOperationIdData(operationId);
-        await this.operationIdService.emitChangeEvent(
-            OPERATION_ID_STATUS.UPDATE.UPDATE_GET_CACHED_OPERATION_ID_DATA_END,
-            operationId,
-            blockchain,
-        );
 
         return {
             dataset,
@@ -53,12 +34,8 @@ class PublishRequestCommand extends ProtocolRequestCommand {
     }
 
     async handleAck(command, responseData) {
-        const { operationId, blockchain } = command.data;
-        await this.operationIdService.emitChangeEvent(
-            OPERATION_ID_STATUS.UPDATE.UPDATE_ADD_SIGNATURE_TO_STORAGE_START,
-            operationId,
-            blockchain,
-        );
+        const { operationId } = command.data;
+
         await this.signatureService.addSignatureToStorage(
             NETWORK_SIGNATURES_FOLDER,
             operationId,
@@ -67,11 +44,6 @@ class PublishRequestCommand extends ProtocolRequestCommand {
             responseData.r,
             responseData.s,
             responseData.vs,
-        );
-        await this.operationIdService.emitChangeEvent(
-            OPERATION_ID_STATUS.UPDATE.UPDATE_ADD_SIGNATURE_TO_STORAGE_END,
-            operationId,
-            blockchain,
         );
 
         return super.handleAck(command, responseData);
