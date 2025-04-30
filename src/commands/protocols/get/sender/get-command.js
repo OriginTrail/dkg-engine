081bd58fda1b38624570cad6d54decc70d469640
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2025-04-30 11:59:26 +0200

    Add GET_FAILED event

--- src/commands/protocols/get/sender/get-command.js
+++ src/commands/protocols/get/sender/get-command.js
@@ -38,6 +38,7 @@ class GetCommand extends Command {
             errorMessage,
             errorType,
         );
+        this.emitChangeEvent(OPERATION_ID_STATUS.GET.GET_FAILED, operationId);
     }
 
     /**
