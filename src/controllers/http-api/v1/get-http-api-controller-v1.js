fb1455816f91b431b62ad219ecba5da3b28cf272
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2025-04-30 12:01:38 +0200

    Fix emitChangeEvent in get controller

--- src/controllers/http-api/v1/get-http-api-controller-v1.js
+++ src/controllers/http-api/v1/get-http-api-controller-v1.js
@@ -107,7 +107,10 @@ class GetController extends BaseController {
                 'Unable to get data, Failed to process input data!',
                 ERROR_TYPE.GET.GET_ROUTE_ERROR,
             );
-            this.emitChangeEvent(OPERATION_ID_STATUS.GET.GET_FAILED, operationId);
+            this.operationIdService.emitChangeEvent(
+                OPERATION_ID_STATUS.GET.GET_FAILED,
+                operationId,
+            );
         }
     }
 }
