fc74793c33af0970e83de4f506661d7c8d87f240
Marko Kostic <marko03kostic@protonmail.com>
2025-05-22 14:16:00 +0200

    added migrations and index

---
+++ src/modules/repository/implementation/sequelize/migrations/20250522124500-create-claim-rewards.js
@@ -0,0 +1,62 @@
+export const up = async ({ context: { queryInterface, Sequelize } }) => {
+    await queryInterface.createTable('epoch_rewards_claimed', {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+        },
+        blockchain_id: {
+            allowNull: false,
+            type: Sequelize.STRING,
+        },
+        epoch: {
+            allowNull: false,
+            type: Sequelize.INTEGER,
+        },
+        claimed: {
+            allowNull: false,
+            type: Sequelize.BOOLEAN,
+            defaultValue: false,
+        },
+        created_at: {
+            allowNull: false,
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.literal('NOW()'),
+        },
+        updated_at: {
+            allowNull: false,
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.literal('NOW()'),
+        },
+    });
+
+    const [[{ indexExists: epochRewardsClaimedIdxEpochRewardsClaimedExists }]] =
+        await queryInterface.sequelize.query(`
+          SELECT COUNT(*) AS indexExists
+          FROM information_schema.statistics
+          WHERE TABLE_SCHEMA = DATABASE()
+          AND TABLE_NAME = 'epoch_rewards_claimed'
+          AND INDEX_NAME = 'idx_epoch_rewards_claimed';
+    `);
+
+    if (!epochRewardsClaimedIdxEpochRewardsClaimedExists) {
+        await queryInterface.addIndex('epoch_rewards_claimed', ['blockchain_id', 'claimed'], {
+            name: 'idx_epoch_rewards_claimed',
+        });
+    }
+};
+
+export const down = async ({ context: { queryInterface } }) => {
+    const [[{ indexExists: epochRewardsClaimedIdxEpochRewardsClaimedExists }]] =
+        await queryInterface.sequelize.query(`
+          SELECT COUNT(*) AS indexExists
+          FROM information_schema.statistics
+          WHERE TABLE_SCHEMA = DATABASE()
+          AND TABLE_NAME = 'epoch_rewards_claimed'
+          AND INDEX_NAME = 'idx_epoch_rewards_claimed';
+    `);
+    if (epochRewardsClaimedIdxEpochRewardsClaimedExists) {
+        await queryInterface.removeIndex('epoch_rewards_claimed', 'idx_epoch_rewards_claimed');
+    }
+    await queryInterface.dropTable('epoch_rewards_claimed');
+};
