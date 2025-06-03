7c62b5346aa713c28bd0dba1dd2fdf1a60ffb5cb
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2025-06-03 11:46:17 +0200

    Move from inf to 15 minutes

--- src/service/sync-service.js
+++ src/service/sync-service.js
@@ -5,6 +5,8 @@ import {
     OPERATION_ID_STATUS,
     DKG_METADATA_PREDICATES,
     TRIPLE_STORE_REPOSITORY,
+    SYNC_BATCH_GET_MAX_ATTEMPTS,
+    SYNC_BATCH_GET_WAIT_TIME,
 } from '../constants/constants.js';
 
 class SyncService {
@@ -460,11 +462,11 @@ class SyncService {
         });
 
         let batchGetResult;
-
+        let attempts = 0;
         // Poll for result
-        while (true) {
+        while (attempts < SYNC_BATCH_GET_MAX_ATTEMPTS) {
             // eslint-disable-next-line no-await-in-loop
-            await setTimeout(1000);
+            await setTimeout(SYNC_BATCH_GET_WAIT_TIME);
             // eslint-disable-next-line no-await-in-loop
             batchGetResult = await this.operationIdService.getOperationIdRecord(
                 batchGetOperationId,
@@ -476,6 +478,7 @@ class SyncService {
             ) {
                 break;
             }
+            attempts += 1;
         }
         return { batchGetResult, batchGetOperationId };
     }
