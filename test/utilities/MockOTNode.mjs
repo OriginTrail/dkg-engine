c49ed129137bfe6e06a1433feb7d027545e4f7fe
Bojan <bojan.colic131@gmail.com>
2026-01-21 11:47:51 +0100

    fix bdd tests

--- test/utilities/MockOTNode.mjs
+++ test/utilities/MockOTNode.mjs
@@ -5,4 +5,32 @@ export default class MockOTNode extends OTNode {
         this.logger.info('[Mock] Skipping startNetworkModule in test');
         // Do nothing
     }
+
+    async stop() {
+        this.logger.info('[Mock] Stopping node...');
+        try {
+            // Stop command executor
+            const commandExecutor = this.container?.resolve('commandExecutor');
+            if (commandExecutor) {
+                await commandExecutor.commandExecutorShutdown();
+            }
+
+            // Stop HTTP server
+            const httpClientModuleManager = this.container?.resolve('httpClientModuleManager');
+            if (httpClientModuleManager?.close) {
+                await httpClientModuleManager.close();
+            }
+
+            // Stop blockchain event listeners
+            const blockchainEventsService = this.container?.resolve('blockchainEventsService');
+            if (blockchainEventsService?.stopListening) {
+                await blockchainEventsService.stopListening();
+            }
+
+            this.logger.info('[Mock] Node stopped successfully');
+        } catch (error) {
+            this.logger.error(`[Mock] Error stopping node: ${error.message}`);
+            throw error;
+        }
+    }
 }
